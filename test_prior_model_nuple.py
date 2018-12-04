0cc2d5f7fb940ffd49aec51f305d47a8376cd9f5
vbuntinx <vincent.buntinx@epfl.ch>
2018-12-04 17:19:35 +0100

    adding the license

--- test_prior_model_nuple.py
+++ test_prior_model_nuple.py
@@ -1,3 +1,26 @@
+#  Scanvan
+#
+#      Vincent Buntinx - vbuntinx@shogaku.ch
+#      Copyright (c) 2016-2018 DHLAB, EPFL
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+    #! \file   test_prior_model_nuple.py
+    #  \author Vincent Buntinx <vbuntinx@shogaku.ch>
+    #
+    #  Scanvan - https://github.com/ScanVan
+
 import numpy as np
 import spherical_algo_nuple
 from plyfile import PlyData, PlyElement
